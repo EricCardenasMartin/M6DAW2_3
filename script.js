285b638e58dd14aeff6f168d178bdeb906315f53
daw2-Cardenas22 <71781302+EricCardenasMartin@users.noreply.github.com>
2021-10-22 12:58:29 +0000

    a

--- script.js
+++
@@ -1,9 +0,0 @@
-var nombreCF = prompt("Introduce el nombre de tu ciclo formativo pls:");
-var nombreM = prompt("Introduce el nombre de una asignatura pls:");
-
-if(nombreCF == "Daw")
-	alert("has introducido el texto Daw");
-else
-	alert(nombreCF.toUpperCase() + " " + nombreM.toLowerCase());
-
-alert(nombreCF + " tiene " + nombreCF.length + " caracteres :D");
