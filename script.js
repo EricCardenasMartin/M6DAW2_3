0b0eda2a3ffee2f76b15805a6db677eb8a701430
daw2-Cardenas22 <71781302+EricCardenasMartin@users.noreply.github.com>
2021-11-05 14:58:20 +0000

    Acabado v1.0 5/11/21

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-var fechaInput = new Date(prompt(`Por favor introduce una fecha en formato dd/mm/aaaa`));
+var fechaInput = prompt(`Por favor introduce una fecha en formato dd/mm/aaaa`);
+fechaInput = fechaInput.split('/');
+fechaInput = new Date(fechaInput[2] < 1000 ? 2000 + parseInt(fechaInput[2]) : fechaInput[2], parseInt(fechaInput[1])-1, fechaInput[0])
 
-document.write(`Formato estandar: ${fechaInput.toLocaleDateString()} `);
-document.write(`Formato estandar: ${fechaInput.toLocaleDateString()} `);
+var diasSemana = ["lunes","martes","miercoles","jueves","viernes","sabado","domingo"];
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+var day,month,year,dayDate;
+
+day = fechaInput.getDay()-1;
+dayDate = fechaInput.getDate();
+month = fechaInput.getMonth()+1;
+year = fechaInput.getFullYear();
+
+document.write(`Formato estandar: ${fechaInput.toString()} `);
+document.write(`${diasSemana[day]}`);
+
+document.write(`<br/>`);
+
+document.write(`${diasSemana[day]}, ${dayDate} de ${meses[month-1]} de ${year} `);
+document.write(`- ${day+1 > 5 ? "Es" : "No es"} fin de semana `);
+
+var isHolidays = (month < 7 || month > 9 || (month == 9 && dayDate > 15)) ? false : true;
+
+document.write(`- ${ isHolidays ? "Es" : "No es "} vacaciones`);
